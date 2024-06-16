e3fb8283bfa6c5edd1af7e8b683fb07b3fee315f
skschaff <skschaffner@gmail.com>
2024-06-15 22:42:27 -0400

    Example 4 - Converting seconds to minutes and seconds.

--- Chapter-2-Examples.cpp
+++ Chapter-2-Examples.cpp
@@ -1,20 +1,22 @@
-// Chapter-2-Examples.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Example 4 - Converting Seconds to Minutes and Seconds
 //
 
 #include <iostream>
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Repository for Chapter 2 Examples\n";
+    int totalSeconds, minutes, seconds;
+
+    cout << "Enter a number of seconds between 0 and 3600: ";
+    cin >> totalSeconds;
+
+    minutes = totalSeconds / 60;
+    seconds = totalSeconds % 60;
+
+    cout << minutes << " minutes and " << seconds << " seconds" << endl;
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
